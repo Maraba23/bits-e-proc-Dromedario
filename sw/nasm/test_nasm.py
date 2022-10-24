528d106ffbb60785a82b4722d845c6fed1fc0104
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2022-10-24 15:16:35 -0300

    vector mean, matriz, sweled2

--- sw/nasm/test_nasm.py
+++ sw/nasm/test_nasm.py
@@ -136,7 +136,19 @@ def test_palindromo():
 def test_linha():
     ram = {}
     tst = {}
-    nasm_test("linha.nasm", ram, tst, 10000)
+    nasm_test("LCDlinha.nasm", ram, tst, 10000)
+
+
+def test_quadrado():
+    ram = {}
+    tst = {}
+    nasm_test("LCDquadrado.nasm", ram, tst, 10000)
+
+
+def test_letra():
+    ram = {}
+    tst = {}
+    nasm_test("LCDletraGrupo.nasm", ram, tst, 10000)
 
 
 def test_factorial():
@@ -144,11 +156,11 @@ def test_factorial():
     tst = {1: math.factorial(ram[0])}
     assert nasm_test("factorial.nasm", ram, tst, 10000)
 
-    ram = {1: 0}
+    ram = {0: 2}
     tst = {1: math.factorial(ram[0])}
     assert nasm_test("factorial.nasm", ram, tst, 10000)
 
-    ram = {1: 4}
+    ram = {0: 4}
     tst = {1: math.factorial(ram[0])}
     assert nasm_test("factorial.nasm", ram, tst, 10000)
 
@@ -165,3 +177,55 @@ def test_multiploDeDois():
     ram = {5: -4}
     tst = {0: 1}
     assert nasm_test("multiploDeDois.nasm", ram, tst, 10000)
+
+
+def test_matrizDeterminante():
+    ram = {1000: 5, 1001: 3, 1003: 1, 1004: 4}
+    tst = {0: 17, 1: 20, 2: 3}
+    assert nasm_test("matrizDeterminante.nasm", ram, tst, 10000)
+
+
+def test_vectorMean():
+    ram = {4:4, 5:1, 6:2, 7:1, 8:8}
+    tst = {0:3, 1:12}
+
+    assert nasm_test("vectorMean.nasm", ram, tst, 10000)
+
+
+def test_SWeLED():
+    ram = {21185: 4}
+    tst = {21184: 506}
+
+    assert nasm_test("SWeLED.nasm", ram, tst, 10000)
+
+    ram = {21185: 12}
+    tst = {21184: 498}
+
+    assert nasm_test("SWeLED.nasm", ram, tst, 10000)
+
+    ram = {21185: 14}
+    tst = {21184: 496}
+
+    assert nasm_test("SWeLED.nasm", ram, tst, 10000)
+
+    ram = {21185: 416}
+    tst = {21184: 510}
+
+    assert nasm_test("SWeLED.nasm", ram, tst, 10000)
+
+    ram = {21185: 165}
+    tst = {21184: 506}
+
+    assert nasm_test("SWeLED.nasm", ram, tst, 10000)
+
+
+def test_SWeLED2():
+    ram = {5: 24, 21185: 409} # 0000000110011001 -> SWs // 0000000000011000 -> RAM[5]
+    tst = {21184: 318} # 0000000100111110 -> LEDs
+
+    assert nasm_test("SWeLED2.nasm", ram, tst, 10000)
+
+    ram = {5: 23, 21185: 24} # 0000000000011000 -> SWs // 0000000000010111 -> RAM[5]
+    tst = {21184: 180} # 0000000010110100 -> LEDs
+
+    assert nasm_test("SWeLED2.nasm", ram, tst, 10000)
